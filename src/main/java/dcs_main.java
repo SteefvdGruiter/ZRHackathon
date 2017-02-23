ff6573cc099838da84806a0881a551b0f588ce1e
Cyrohn <viss0135@hz.nl>
2017-02-23 15:42:07 +0100

    first launchable screen.

---
+++ src/main/java/dcs_main.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javax.swing.*;
+
+public class dcs_main extends Application {
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
